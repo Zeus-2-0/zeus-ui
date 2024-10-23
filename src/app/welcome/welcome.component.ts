4594e0713eba31881a1edf55ae4d8f654360860c
vbalaji215-dev <vbalaji215@outlook.com>
2024-10-23 10:03:39 -0400

    Added welcome component

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zeus-welcome',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent {
+
+}
