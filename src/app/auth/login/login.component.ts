bee170344b58f1f1e81c4dc7c979a04aca020af3
vbalaji215-dev <vbalaji215@outlook.com>
2024-12-18 16:19:35 -0500

    Added login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, inject, Signal, viewChild} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {FormsModule, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'zeus-login',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginErrorMessage: string = '';
+  private form: Signal<NgForm>= viewChild.required<NgForm>('loginForm');
+  private destroyRef = inject(DestroyRef)
+
+  onLogin(): void{
+    console.log("user clicked login")
+  }
 }
